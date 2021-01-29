2745d5642a5132d65537e9b967419582a869dc00
Carlos <agazor1998@gmail.com>
2021-01-29 11:56:05 +0000

    Scund TDD cicle The quantity is analyzed correctly

--- src/test/java/test/Analizer_.java
+++ src/test/java/test/Analizer_.java
@@ -4,28 +4,72 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class Analizer_ {
 
     Analizer analizer;
-
+    int quantity;
     @Before
     public void init(){
         analizer = new Analizer();
     }
 
     @Test
-    public void should_return_the_quantity_of() {
-        int a = analizer.analyze(1000, 2000, 3)[0];
-        assertThat(11==a);
-
+    public void should_return_other_quantity() {
+        quantity = analizer.analyze(1000, 2000, 3)[0];
+        assertTrue(11==quantity);
+        quantity = analizer.analyze(1000, 2000, 4)[0];
+        assertTrue(21==quantity);
+        quantity = analizer.analyze(1000, 2000, 7)[0];
+        assertTrue(85==quantity);
+        quantity = analizer.analyze(1000, 3000, 7)[0];
+        assertTrue(141==quantity);
     }
 
+
     public class Analizer {
-        public int[] analyze(int i, int i1, int i2) {
+        public int[] analyze(int i0, int i1, int i2) {
             int[] r = new int[2];
-            r[0]= 11;
+            r[0]= 0;
             r[1]= 0;
+            for(int i = i0; i<=i1; i++){
+                if(sumOfDigitsAreGreater(i, i2)){
+                    r[0]++;
+                }
+            }
+            return r;
+        }
+
+        private boolean sumOfDigitsAreGreater(int i, int maxSum) {
+            int[] digits;
+            if(i<10000){
+                digits = new int[4];
+                for (int j = 0; j<3; j++){
+                    digits[j]=i%10;
+                    i = i/10;
+                }
+                return sum(digits)<maxSum;
+            }else{
+                digits = new int[5];
+                for (int j = 0; j<4; j++){
+                    digits[j]=i%10;
+                    i = i/10;
+                }
+                int[] digits2=new int[4];
+                for (int j = 0; j<3; j++){
+                    digits2[j]=digits[j+1];
+                }
+                return sum(digits)<maxSum||sum(digits2)<maxSum;
+            }
+
+        }
+
+        private int sum(int[] digits) {
+            int r = 0;
+            for(int i=0; i<3; i++){
+                r += digits[i];
+            }
             return r;
         }
     }
